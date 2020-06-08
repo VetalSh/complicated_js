6c729f3e007b89da6fb826da61b0ed956dc8a60d
VetalSh <jedvemya86@gmail.com>
2020-06-08 21:08:46 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let myVar = 266219;
+console.log('Создаем переменную myVar: ', myVar);
+console.log('Тип этой переменной: ', typeof myVar);
+
+let myArr = myVar.toString().split('');
+console.log('Разбиваем переменную на отдельные числа: ', myArr);
+
+let mult = 1;
+for (let i = 0; i < myArr.length; i++) {
+  mult *= +myArr[i];
+}
+console.log('Умножаем числа между собой: ', mult);
+mult **= 3;
+console.log('Результат возводим в степень 3: ', mult);
+
+let result = mult.toString();
+console.log('Выводим на экран 2 первых цифры: ', result.slice(0, 2));
+alert(result.slice(0, 2));
